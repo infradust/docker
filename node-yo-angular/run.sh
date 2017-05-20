d4b51d5c3011256fd3b9710b51d2c7f8c6a0997e
Dan Shelly <danshelly@dans-mbp.mynet>
2017-05-20 20:15:01 +0300

    reuse node user from origin container

--- node-yo-angular/run.sh
+++ node-yo-angular/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --name yo-dev -v `pwd`:/home/yeoman/app -p 9000:9000 -p 35729:35729 infradust/node-yo-angular:0.1
+docker run -it --rm --name yo-dev -v `pwd`:/app -p 9000:9000 -p 35729:35729 infradust/node-yo-angular:0.1
